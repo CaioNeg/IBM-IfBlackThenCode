61fc65af6aed8f10cc0af4e337c65a521890cfe7
CaioNegrao <caio_negrao0210@outlook.com>
2022-08-22 11:35:22 -0300

    iniciando projeto com spring

---
+++ console/src/main/java/com/br/caio/console/LogicaApplication.java
@@ -0,0 +1,15 @@
+package com.br.caio.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println(x:"Bem vindo ao app java console");
+		//SpringApplication.run(LogicaApplication.class, args);
+	}
+
+}
